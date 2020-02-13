6deed78f1d5633634959734806affdee245661eb
hadarozer10 <ozerhadar10@gmail.com>
2020-02-13 13:01:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const app = express();
 const winston = require("winston");
+const https = require("https");
+const fs = require("fs");
+const WebSocket = require("ws");
 
+const port = process.env.PORT || 3000;
+
+require("./startup/db")();
+require("./startup/redis")(app);
+require("./startup/cors")(app);
 require("./startup/logging")();
 require("./startup/routes")(app);
-require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
-require("./startup/cors")(app);
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
-);
+app.disable("x-powered-by");
+
+const httpsOptions = {
+  key: fs.readFileSync("ssl/key.pem"),
+  cert: fs.readFileSync("ssl/key-cert.pem")
+};
+
+const server = https.createServer(httpsOptions, app).listen(port, () => {
+  winston.info(`Listening on port ${port}...`);
+});
+
+const wss = new WebSocket.Server({ server });
+// wss.on("connection", function connection(ws) {
+//   ws.on("message", function incoming(message) {
+//     console.log("received: %s", message);
+//   });
+
+//   ws.send("something");
+// });
 
 module.exports = server;
